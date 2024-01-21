eec25a89900c49d78553d9ccfcfc334453a91703
Margaryta Filchakova <m.filchakova89@gmail.com>
2024-01-21 23:02:10 +0100

    change script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// let temperature = 18;
-// let isWindy = true;
-
-// if (temperature < 15 && isWindy){
-    // console.log("Вдягніться тепліше і візьміть шапку");
-// }
-// else if(temperature > 15) {
-    // console.log("Хороший день для прогулянки");
-// }
-
-
-//for (let i = 1; i < 11; i++){
-    //console.log("Iteration" + i);
-//}
-
-
 
 let vegetables = ["carrot", "potato", "cucumber"];
 vegetables.push('tomato');
